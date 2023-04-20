aa9c379ac44450c7d8c231cc1f7c90ec7ba46c8a
Rajan Agarwal <rajan.ag005@gmail.com>
2023-04-19 23:53:13 -0400

    Initial Commit

---
+++ Zephyr/ZephyrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZephyrApp.swift
+//  Zephyr
+//
+//  Created by Rajan Agarwal on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct ZephyrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
